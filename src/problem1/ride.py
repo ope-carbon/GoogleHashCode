8b6a1820b6997f965c7dec771d9ed8ca59d1f489
Krishan Patel <krishan.patel@carbonnv.com>
2018-03-01 18:14:38 +0000

    .

--- src/problem1/ride.py
+++ src/problem1/ride.py
@@ -1,7 +1,8 @@
 
 class Ride:
 
-    def __init__(self, startPosition, endPosition, earliestStartTime, latestFinishTime, bonusFactor):
+    def __init__(self, rideId, startPosition, endPosition, earliestStartTime, latestFinishTime, bonusFactor):
+        self.rideId = rideId
         self.startPosition = startPosition
         self.endPosition = endPosition
         self.earliestStartTime = earliestStartTime
