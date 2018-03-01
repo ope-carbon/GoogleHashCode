a46579a788bf222f14d32145ee33a835134007fe
Krishan Patel <krishan.patel@carbonnv.com>
2018-03-01 20:25:05 +0000

    .

--- src/problem1/processor.py
+++ src/problem1/processor.py
@@ -23,29 +23,41 @@ class Processor:
             return 0
         startTimeFromNow = ride.earliestStartTime - vehicle.currentTime
         waitTime = max(vehicle.position.distance_to(ride.startPosition) - startTimeFromNow, 0)
-        rideCannotFinish = (ride.earliestStartTime + waitTime + ride.length) > ride.latestFinishTime
+        rideFinishTime = ride.earliestStartTime + waitTime + ride.length
+        rideCannotFinish = rideFinishTime > ride.latestFinishTime
         if rideCannotFinish:
+            return 0
+        rideFinishesAfterSteps = rideFinishTime >= steps
+        if rideFinishesAfterSteps:
             return 0
         return ride.length + (bonusFactor if waitTime == 0 else 0)
 
-    def update_matrix(self, matrix, vehicles, rides):
-        pass
+    def update_matrix(self, matrix, vehicles, rides, steps, bonusFactor):
+        for vehicleIndex, vehicle in enumerate(vehicles):
+            for rideIndex, ride in enumerate(rides):
+                matrix[vehicleIndex][rideIndex] = self.get_score(vehicle=vehicle, ride=ride, steps=steps, bonusFactor=bonusFactor)
+
 
     def process(self, input_):
+        vehicles = np.array(input_.vehicles)
+        rides = np.array(input_.rides)
+        matrix = np.zeros((len(vehicles), len(rides)))
         while True:
-            vehicles = np.array(input_.vehicles)
-            rides = np.array(input_.rides)
-            matrix = np.zeros((len(vehicles), len(rides)))
             print(matrix)
-            self.update_matrix(matrix, vehicles, rides)
+            self.update_matrix(matrix, vehicles, rides, input_.steps, input_.bonusFactor)
             print(matrix)
             vehicleIndex, rideIndex = np.unravel_index(matrix.argmax(), matrix.shape)
-            ride = rides[rideIndex]
-            vehicle = vehicle[vehicleIndex]
-            ride.completed = True
-            vehicle.currentTime =
             print(vehicleIndex, rideIndex)
-
+            maxScore = matrix[vehicleIndex][rideIndex]
+            if maxScore == 0:
+                break
+            ride = rides[rideIndex]
+            vehicle = vehicles[vehicleIndex]
+            print(vehicle, ride)
+            ride.completed = True
+            vehicle.do_ride(ride)
+        for vehicle in vehicles:
+            print(vehicle, ridesCompleted)
 
 
         # availableRides = set(input_.rides)
