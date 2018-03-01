9408f5dea2c582e4897a2db6cf66051831d72b1d
Krishan Patel <krishan.patel@carbonnv.com>
2018-03-01 20:30:59 +0000

    .

--- src/problem1/processor.py
+++ src/problem1/processor.py
@@ -3,21 +3,6 @@ import numpy as np
 
 class Processor:
 
-    def find_best_ride(self, currentTime, timeLeft, vehicle, availableRides, bonusFactor):
-        bestRide = None
-        highestPoints = 0
-        for ride in availableRides:
-            startTimeFromNow = ride.earliestStartTime - currentTime
-            waitTime = vehicle.position.distance_to(ride.startPosition) - startTimeFromNow
-            if waitTime + ride.length >= ride.latestFinishTime:
-                # print(vehicle, 'cant finish', ride)
-                continue
-            points = ride.length + (bonusFactor if waitTime == 0 else 0)
-            if points > highestPoints:
-                highestPoints = points
-                bestRide = ride
-        return bestRide
-
     def get_score(self, vehicle, ride, steps, bonusFactor):
         if ride.completed:
             return 0
@@ -43,38 +28,18 @@ class Processor:
         rides = np.array(input_.rides)
         matrix = np.zeros((len(vehicles), len(rides)))
         while True:
-            print(matrix)
+            # print(matrix)
             self.update_matrix(matrix, vehicles, rides, input_.steps, input_.bonusFactor)
-            print(matrix)
+            # print(matrix)
             vehicleIndex, rideIndex = np.unravel_index(matrix.argmax(), matrix.shape)
-            print(vehicleIndex, rideIndex)
+            # print(vehicleIndex, rideIndex)
             maxScore = matrix[vehicleIndex][rideIndex]
             if maxScore == 0:
                 break
             ride = rides[rideIndex]
             vehicle = vehicles[vehicleIndex]
-            print(vehicle, ride)
+            # print(vehicle, ride)
             ride.completed = True
             vehicle.do_ride(ride)
-        for vehicle in vehicles:
-            print(vehicle, ridesCompleted)
-
-
-        # availableRides = set(input_.rides)
-        # vehicleNextAvailable = {vehicle: 0 for vehicle in input_.vehicles}
-        # time = 0
-        # while time < input_.steps:
-        #     if len(availableRides) == 0:
-        #         break
-        #     availableVehicles = {vehicle for vehicle, availableTime in vehicleNextAvailable.items() if availableTime <= time}
-        #     for vehicle in availableVehicles:
-        #         ride = self.find_best_ride(currentTime=time, timeLeft=input_.steps-time, vehicle=vehicle, availableRides=availableRides, bonusFactor=input_.bonusFactor)
-        #         # print(vehicle, ride)
-        #         if not ride:
-        #             continue
-        #         availableRides.remove(ride)
-        #         vehicle.position = ride.endPosition
-        #         vehiclesRides[vehicle].append(ride)
-        #         vehicleNextAvailable[vehicle] = ride.length + max(vehicle.position.distance_to(ride.startPosition), ride.earliestStartTime)
-        #     time += 1
-        return Output(vehiclesRides={})
+        vehiclesRides = {vehicle.vehicleId: vehicle.ridesCompleted for vehicle in vehicles}
+        return Output(vehiclesRides=vehiclesRides)
