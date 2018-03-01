f0420fd3d02ce4086fbbaa29339b9be9b7e82e82
Rikhil Raithatha <rikhil@carbonnv.com>
2018-03-01 20:27:37 +0000

    .

--- src/problem1/vehicle.py
+++ src/problem1/vehicle.py
@@ -5,6 +5,7 @@ class Vehicle:
         self.vehicleId = vehicleId
         self.position = position
         self.currentTime = currentTime
+        self.ridesCompleted = []
 
     def __eq__(self, other):
         return self.vehicleId == other.vehicleId
@@ -17,3 +18,8 @@ class Vehicle:
 
     def __str__(self):
         return self.__repr__()
+
+    def do_ride(self, ride):
+        self.currentTime = max(self.currentTime + self.position.distance_to(ride.startPosition), ride.earliestStartTime) + ride.length
+        self.position = ride.endPosition
+        self.ridesCompleted.append(ride)
