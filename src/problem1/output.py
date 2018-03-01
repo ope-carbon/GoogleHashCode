a99d3b1857f3d8452f875e0d4e31d36d7823062c
Krishan Patel <krishan.patel@carbonnv.com>
2018-03-01 18:49:25 +0000

    .

--- src/problem1/output.py
+++ src/problem1/output.py
@@ -6,7 +6,7 @@ class Output:
         self.vehiclesRides = vehiclesRides
 
     def write_to_file(self, outputFile):
-        with open(self.outputFile, 'w') as solFile:
+        with open(outputFile, 'w') as solFile:
             for vehicle, rides in self.vehiclesRides.items():
                 outputText = '{vehicle} {rides}\n'.format(vehicle=vehicle.vehicleId, rides=' '.join([ride.rideId for ride in rides]))
                 solFile.write(outputText)
