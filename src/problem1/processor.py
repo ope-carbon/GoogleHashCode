6e377ddb46dd8ba99ad160c2c359b0fd394f4acc
Krishan Patel <krishan.patel@carbonnv.com>
2018-03-01 19:17:19 +0000

    .

--- src/problem1/processor.py
+++ src/problem1/processor.py
@@ -2,13 +2,17 @@ from .output import Output
 
 class Processor:
 
-    def find_best_ride(self, vehicle, availableRides):
+    def find_best_ride(self, currentTime, timeLeft, vehicle, availableRides, bonusFactor):
         bestRide = None
-        bestTime = 1000000000000000000
+        highestPoints = 0
         for ride in availableRides:
-            time = ride.length + max(vehicle.position.distance_to(ride.startPosition), ride.earliestStartTime)
-            if time < bestTime:
-                bestTime = time
+            startTimeFromNow = ride.earliestStartTime - currentTime
+            waitTime = startTimeFromNow - vehicle.position.distance_to(ride.startPosition)
+            if waitTime + ride.length > ride.lastestFinishTime:
+                continue
+            points = ride.length + bonusFactor * (1 if waitTime == 0 else 0) +
+            if points > highestPoints:
+                highestPoints = points
                 bestRide = ride
         return bestRide
 
@@ -18,9 +22,11 @@ class Processor:
         vehicleNextAvailable = {vehicle: 0 for vehicle in input_.vehicles}
         time = 0
         while time < input_.steps:
+            if len(availableRides) == 0:
+                break
             availableVehicles = {vehicle for vehicle, availableTime in vehicleNextAvailable.items() if availableTime <= time}
             for vehicle in availableVehicles:
-                ride = self.find_best_ride(vehicle=vehicle, availableRides=availableRides)
+                ride = self.find_best_ride(currentTime=time, timeLeft=input_.steps-time, vehicle=vehicle, availableRides=availableRides, bonusFactor=input_.bonusFactor)
                 if not ride:
                     continue
                 availableRides.remove(ride)
--- src/run.py
+++ src/run.py
@@ -6,6 +6,7 @@ from problem1.processor import Processor
 problems = ['a', 'b', 'c', 'd', 'e']
 
 for problem in problems:
+    print(problem)
     input_ = Input.from_file('input_{}.txt'.format(problem))
     output = Processor().process(input_=input_)
     output.write_to_file(outputFile='output_{}.txt'.format(problem))
