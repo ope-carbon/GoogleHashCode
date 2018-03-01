acc3a2428bdc856021b39bf24e15515fd591fdb1
Krishan Patel <krishan.patel@carbonnv.com>
2018-03-01 18:00:36 +0000

    .

--- problem1/run.py
+++ problem1/run.py
@@ -3,10 +3,7 @@
 problems = ['a', 'b', 'c', 'd', 'e']
 
 for problem in problems:
-    inputFilename = 'input_{}.txt'.format(problem)
-    # Load data from file to make a grid class
-
-
-
+    _input = Input.fromFile('input_{}.txt'.format(problem))
+    output = Processor().process(input=_input)
     outputFilename = 'output_{}.txt'.format(problem)
     # Create output file
