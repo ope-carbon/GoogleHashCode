245f68b8c774f3bd2382a3bfad7539269fd0c6ac
Rikhil Raithatha <rikhil@carbonnv.com>
2018-03-01 18:42:42 +0000

    .

--- src/problem1/input.py
+++ src/problem1/input.py
@@ -24,6 +24,7 @@ class Input:
         with open(filename, 'r+') as file:
             content = file.readlines()
         meta = content[0].strip().split()
+        bonusFactor = meta[4]
         noOfRides = meta[3]
         grid = Grid(rows=meta[0], columns=meta[1])
         rawRides = content[1:]
@@ -33,9 +34,9 @@ class Input:
         rideId = 0
         for rawRide in rawRides:
             rawRide = rawRide.strip().split()
-            rides.append(Ride(rideId=rideId, startPosition=Position(row=rawRide[0], column=rawRide[1]), endPosition=Position(row=rawRide[2], column=rawRide[3]), earliestStartTime=rawRide[4], latestFinishTime=rawRide[5], bonusFactor=rawRide[0]))
+            rides.append(Ride(rideId=rideId, startPosition=Position(row=rawRide[0], column=rawRide[1]), endPosition=Position(row=rawRide[2], column=rawRide[3]), earliestStartTime=rawRide[4], latestFinishTime=rawRide[5], bonusFactor=bonusFactor))
             rideId += 1
         vehicles = []
         for vehicleId in range(int(meta[2])):
             vehicles.append(Vehicle(vehicleId=vehicleId, position=Position(row=0, column=0)))
-        return cls(grid=grid, vehicles=vehicles, rides=rides, bonusFactor=meta[4], steps=meta[5])
+        return cls(grid=grid, vehicles=vehicles, rides=rides, bonusFactor=bonusFactor, steps=meta[5])
